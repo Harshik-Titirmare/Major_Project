f4aaaed2d29556086460f017bdbec4ee9e5787a0
Harshik Titirmare <harshiktitirmare@gmail.com>
2023-09-22 18:08:48 +0530

    create a splash screen and login screen

---
+++ app/src/main/java/com/example/wasteguard/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.wasteguard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 5000;
+
+    //variables
+    Animation topAnim, bottomAnim;
+    ImageView image, image1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Animations
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        //Hooks
+        image = findViewById(R.id.imageView2);
+        image1 = findViewById(R.id.mini_Image);
+
+        image.setAnimation(topAnim);
+        image1.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,Login.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+
+
+    }
+}
